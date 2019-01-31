f05eefa5726a27c8f899e133e89603a84e68c2cf
ernest.vanduyne <ernie.vanduyne@procentive.com>
2019-01-31 13:47:42 -0600

    Added EC2 role
    WORKS!

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+// https://www.itdiversified.com/comparing-toolsets-for-automating-cloud-infrastructure/
+
 // Set the provider
 provider "aws" {
   region = "us-east-1"
@@ -16,7 +18,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-//Create the DMZ Subnet
+//Create the DMZ Subnet1a
 resource "aws_subnet" "DmzSubnet1a" {
   availability_zone       = "us-east-1a"
   vpc_id                  = "${aws_vpc.main.id}"
@@ -27,6 +29,7 @@ resource "aws_subnet" "DmzSubnet1a" {
   }
 }
 
+//Create the DMZ Subnet1b
 resource "aws_subnet" "DmzSubnet1b" {
   availability_zone       = "us-east-1b"
   vpc_id                  = "${aws_vpc.main.id}"
@@ -37,6 +40,7 @@ resource "aws_subnet" "DmzSubnet1b" {
   }
 }
 
+//Create the Public Subnet1a
 resource "aws_subnet" "PublicSubnet1a" {
   availability_zone       = "us-east-1a"
   vpc_id                  = "${aws_vpc.main.id}"
@@ -47,6 +51,7 @@ resource "aws_subnet" "PublicSubnet1a" {
   }
 }
 
+//Create the Public Subnet1b
 resource "aws_subnet" "PublicSubnet1b" {
   availability_zone       = "us-east-1b"
   vpc_id                  = "${aws_vpc.main.id}"
@@ -57,6 +62,7 @@ resource "aws_subnet" "PublicSubnet1b" {
   }
 }
 
+//Create the Private Subnet1a
 resource "aws_subnet" "PrivateSubnet1a" {
   availability_zone       = "us-east-1a"
   vpc_id                  = "${aws_vpc.main.id}"
@@ -67,6 +73,7 @@ resource "aws_subnet" "PrivateSubnet1a" {
   }
 }
 
+//Create the Private Subnet1b
 resource "aws_subnet" "PrivateSubnet1b" {
   availability_zone       = "us-east-1b"
   vpc_id                  = "${aws_vpc.main.id}"
@@ -77,6 +84,7 @@ resource "aws_subnet" "PrivateSubnet1b" {
   }
 }
 
+//Create the main route table
 resource "aws_route_table" "mainRouteTable" {
   vpc_id                  = "${aws_vpc.main.id}"
   tags = {
@@ -85,6 +93,7 @@ resource "aws_route_table" "mainRouteTable" {
   }
 }
 
+//Create the internet gateway
 resource "aws_internet_gateway" "InternetGateway" {
   vpc_id = "${aws_vpc.main.id}"
   tags = {
@@ -93,9 +102,161 @@ resource "aws_internet_gateway" "InternetGateway" {
   }
 }
 
+//Create the public route
 resource "aws_route" "PublicRoute" {
   route_table_id = "${aws_route_table.mainRouteTable.id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = "${aws_internet_gateway.InternetGateway.id}"
+}
+
+//Create the DMZ route table
+resource "aws_route_table" "DmzRouteTable" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags = {
+    Name                  ="DmzRouteTable--TF"
+    Environment           = "${var.EnvironmntName}"
+  }
+}
+
+//Create the DMZ public route
+resource "aws_route" "DmzPublicRoute" {
+  route_table_id = "${aws_route_table.DmzRouteTable.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.InternetGateway.id}"
+}
+
+//Associate the DMZ Subnets to the DMZ route table  AWS::EC2::SubnetRouteTableAssociation
+resource "aws_route_table_association" "DmzRouteAssociation1a" {
+  route_table_id = "${aws_route_table.DmzRouteTable.id}"
+  subnet_id = "${aws_subnet.DmzSubnet1b.id}"
+}
+resource "aws_route_table_association" "DmzRouteAssociation1b" {
+  route_table_id = "${aws_route_table.DmzRouteTable.id}"
+  subnet_id = "${aws_subnet.DmzSubnet1b.id}"
+}
+
+//Create the public route table
+resource "aws_route_table" "PublicRouteTable" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags = {
+    Name                  ="PublicRouteTable--TF"
+    Environment           = "${var.EnvironmntName}"
+  }
+}
+
+//Create the public route
+resource "aws_route" "PublicPublicRoute" {
+  route_table_id = "${aws_route_table.PublicRouteTable.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.InternetGateway.id}"
+}
+
+//Associate the Public Subnets to the public route table - AWS::EC2::SubnetRouteTableAssociation
+resource "aws_route_table_association" "PublicRouteAssociation1a" {
+  route_table_id = "${aws_route_table.PublicRouteTable.id}"
+  subnet_id = "${aws_subnet.PublicSubnet1a.id}"
+}
+resource "aws_route_table_association" "PublicRouteAssociation1b" {
+  route_table_id = "${aws_route_table.PublicRouteTable.id}"
+  subnet_id = "${aws_subnet.PublicSubnet1b.id}"
+}
+
+// Create the elastic IP for NAT 1
+resource "aws_eip" "NatGateway1EIP" {
+  depends_on = ["aws_internet_gateway.InternetGateway"]
+  vpc = true
+}
+
+// Create the elastic IP for NAT 2
+resource "aws_eip" "NatGateway2EIP" {
+  depends_on = ["aws_internet_gateway.InternetGateway"]
+  vpc = true
+}
+
+// Create the gateway for NAT 1
+resource "aws_nat_gateway" "NatGatewayAZ1" {
+  allocation_id = "${aws_eip.NatGateway1EIP.id}"
+  subnet_id = "${aws_subnet.PublicSubnet1a.id}"
+  tags = {
+    Name                  ="NatGatewayAZ1--TF"
+    Environment           = "${var.EnvironmntName}"
+  }
+}
+// Create the gateway for NAT 2
+resource "aws_nat_gateway" "NatGatewayAZ2" {
+  allocation_id = "${aws_eip.NatGateway2EIP.id}"
+  subnet_id = "${aws_subnet.PublicSubnet1b.id}"
+  tags = {
+    Name                  ="NatGatewayAZ2--TF"
+    Environment           = "${var.EnvironmntName}"
+  }
+}
+
+//Create the private route table for availability zone 1
+resource "aws_route_table" "PrivateRouteTableAZ1" {
+  vpc_id = "${aws_vpc.main.id}"
+    tags = {
+    Name                  ="PrivateRouteTableAZ1--TF"
+    Environment           = "${var.EnvironmntName}"
+  }
+}
+
+//Create the private route for availability zone 2
+resource  "aws_route" "PrivateNatRoute1" {
+  route_table_id = "${aws_route_table.PrivateRouteTableAZ1.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = "${aws_nat_gateway.NatGatewayAZ1.id}"
+}
+
+//Create the private route association for availability zone 1
+resource "aws_route_table_association" "PrivateSubnet1aRouteTbleAsociation" {
+  route_table_id = "${aws_route_table.PrivateRouteTableAZ1.id}"
+  subnet_id = "${aws_subnet.PrivateSubnet1a.id}"
+}
+
+//Create the private route table for availability zone 1
+resource "aws_route_table" "PrivateRouteTableAZ2" {
+  vpc_id = "${aws_vpc.main.id}"
+    tags = {
+    Name                  ="PrivateRouteTableAZ2--TF"
+    Environment           = "${var.EnvironmntName}"
+  }
+}
+
+//Create the private route for availability zone 2
+resource  "aws_route" "PrivateNatRoute2" {
+  route_table_id = "${aws_route_table.PrivateRouteTableAZ2.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = "${aws_nat_gateway.NatGatewayAZ2.id}"
+}
+
+//Create the private route association for availability zone 1
+resource "aws_route_table_association" "PrivateSubnet1bRouteTbleAsociation" {
+  route_table_id = "${aws_route_table.PrivateRouteTableAZ2.id}"
+  subnet_id = "${aws_subnet.PrivateSubnet1b.id}"
+}
+
+// Create IAM role and policy
+resource "aws_iam_role" "Ec2Role" {
+  name = "Ec2Role"
+  path = "/"
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+         "Statement": [
+          {
+            "Effect": "Allow",
+            "Principal": {
+              "Service": [
+                "ec2.amazonaws.com"
+              ]
+            },
+            "Action": [
+              "sts:AssumeRole"
+            ]
+          }
+        ]
+  }
+  EOF
 }
 
